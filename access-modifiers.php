39398e647da32f8183c0e61cc5ef9afa85d1e312
AkramHoshen <akramhoshen@logicsoftbd.com>
2025-05-23 12:33:00 +0600

    access modifiers

---
+++ access-modifiers.php
@@ -0,0 +1,37 @@
+<?php 
+
+class BankAccount {
+
+    //public $balance = 0; accessable
+    //private $balance = 0; //only access in this class
+    protected $balance = 0; //only access in this class and child class
+
+    public function deposit($amount){  //setter
+        if($amount > 0){
+            $this->balance += $amount;
+        }
+    }
+
+    public function getBalance(){ //getter
+        return $this->balance;
+    }
+}
+
+class subAccount extends BankAccount{
+    public function WithDraw($amount){
+        $this->balance -= $amount;
+    }
+}
+
+$account = new BankAccount();
+$account->deposit(500);
+//$account->balance = 500; //not access directly if private and protected
+echo $account->getBalance();
+
+echo "\n";
+
+//child instance
+$subaccount = new subAccount();
+$subaccount->deposit(100); //deposite blance assign
+$subaccount->WithDraw(50);
+echo $subaccount->getBalance(); //now showing withdraw adjust update balance
